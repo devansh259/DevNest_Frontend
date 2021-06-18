636d4d0e5d2bcaf869a0c76b7c26af8667ed81d5
DevanshSharma25 <69204403+DevanshSharma25@users.noreply.github.com>
2021-06-18 17:09:59 +0530

    Create q3.js

---
+++ Day6/q3.js
@@ -0,0 +1,12 @@
+function first(arr,n){
+var array=arr;
+if(n==null){return (array[0]);}
+else if(n<0){return([])}
+else {return array.slice(0,n);}
+
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));

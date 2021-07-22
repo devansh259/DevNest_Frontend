8b95c828f711d77bfdc83b3ea2a53379513ba74f
devansh259 <42205065+devansh259@users.noreply.github.com>
2021-07-23 01:20:39 +0530

    Add files via upload

---
+++ day12/index.js
@@ -0,0 +1,58 @@
+const input = document.querySelector(".input");
+const elements = document.querySelector("ul");
+var doneBtn=document.querySelector("donebtn");
+var deleteBtn= document.querySelector("delete");
+
+var todos={
+    todo:localStorage.getItem('todos')
+};
+
+
+function loadTodos()
+{
+    console.log("Loaded");
+    elements.innerHTML=todos.todo;
+}
+
+loadTodos();
+
+
+
+input.addEventListener('keydown',(event)=>{
+    if(event.keyCode===13)
+    {
+        console.log("Pressed");  // Enter keyCode === 13
+        if(input.value!=="")
+        {
+            let newElement = `<li><div class="delete"><i class="fa fa-trash deleteBtn"></i></div><div class="addedTodo">${input.value}</div></li>`;
+            console.log(input.value)
+            elements.innerHTML+= newElement;
+
+            localStorage.setItem('todos',elements.innerHTML);
+            // input.value=" ";
+        }
+        doneBtn=document.querySelector(".donebtn");
+        todos.todo=localStorage.getItem('todos');
+        loadTodos();
+    }
+});
+
+elements.addEventListener('click',(e)=>{
+    if(e.target.tagName=="LI")
+    {
+        
+        e.target.childNodes[1].classList.toggle("done");
+        localStorage.setItem('todos',elements.innerHTML);
+    }
+    if(e.target.classList.contains("addedTodo"))
+    {
+        e.target.classList.toggle("done");
+        localStorage.setItem('todos',elements.innerHTML);
+    }
+
+    if(e.target.classList.contains("delete")|| e.target.classList.contains("deletebtn"))
+    {
+        e.target.parentElement.remove();
+        localStorage.setItem('todos',elements.innerHTML);
+    }
+});

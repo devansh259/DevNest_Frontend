faeb5d4f80373cba4a6480e8d32ea6e87bb7717a
devansh259 <42205065+devansh259@users.noreply.github.com>
2021-08-02 04:24:04 +0530

    Add files via upload

---
+++ day16/src/components/card.jsx
@@ -0,0 +1,11 @@
+const Card =() =>{
+    return (
+        <div className="card">
+            <img src="https://i.redd.it/ks9boperc3471.jpg" alt="Meme" />
+            <h2>React Card</h2>
+            <p>This is a Meme card</p>
+        </div>
+    );
+};
+
+export default Card;
